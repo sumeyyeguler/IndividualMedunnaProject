f9746ac230bff003d832d8ce63d68ebad204114c
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-11-17 17:43:57 +0300

    sumeyye

---
+++ src/test/java/stepDefinitions/ui_stepDefinitions/US001.java
@@ -0,0 +1,41 @@
+package stepDefinitions.ui_stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US001 {
+
+
+    @Given("Kullanici medunna sayfasina gider")
+    public void kullaniciMedunnaSayfasinaGider() {
+
+    }
+
+    @When("Kullanici sag ustte yer alan account ikonuna tiklar")
+    public void kullaniciSagUstteYerAlanAccountIkonunaTiklar() {
+    }
+
+    @And("Kullanici register butonuna tiklar")
+    public void kullaniciRegisterButonunaTiklar() {
+    }
+
+    @And("Kullanici gecerli {string} girer")
+    public void kullaniciGecerliGirer(String arg0) {
+    }
+
+    @Then("Kullanici {string} yazisinin gorunmedigini test eder")
+    public void kullaniciYazisininGorunmediginiTestEder(String arg0) {
+    }
+
+    @And("Kullanici gecersiz {string} girer")
+    public void kullaniciGecersizGirer(String arg0) {
+    }
+
+    @Then("Kullanici {string} yazisinin gorunur oldugunu test eder")
+    public void kullaniciYazisininGorunurOldugunuTestEder(String arg0) {
+    }
+
+
+}
---
+++ src/test/resources/features/US001.feature
@@ -0,0 +1,27 @@
+@wip
+Feature: US001 Register
+
+
+  Background: US001 ortak adimlar
+    Given Kullanici medunna sayfasina gider
+    When Kullanici sag ustte yer alan account ikonuna tiklar
+    And Kullanici register butonuna tiklar
+
+
+  Scenario Outline: TC01 SSN girisleri Positive
+    And Kullanici gecerli "<SSN>" girer
+    Then Kullanici "<uyari>" yazisinin gorunmedigini test eder
+    Examples:
+      | SSN        | uyari |
+      | 124-23-555 |       |
+      | 124-78-500 |       |
+      | 129-73-342 |       |
+
+  Scenario Outline: TC02 SSN girisleri Negative
+    And Kullanici gecersiz "<SSN>" girer
+    Then Kullanici "<uyari>" yazisinin gorunur oldugunu test eder
+    Examples:
+      | SSN         | uyari                 |
+      | 12423555    | Your SSN is required. |
+      | 124-78500   | Your SSN is required. |
+      | 129-73-3421 | Your SSN is required. |
